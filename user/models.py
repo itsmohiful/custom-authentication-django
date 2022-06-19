0fd48f9d0172792601780088172fa556e3fd0762
Soyad Mohiful Islam <mohiful.islam.ofc@gmail.com>
2022-06-19 20:27:50 +0600

    create custom model UserManager & User, add urls file in user_app

--- user/models.py
+++ user/models.py
@@ -1,3 +1,58 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None):
+        """
+            create and saves user with the given email and password
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+        
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+    
+    def create_superuser(self, email, password=None):
+        """
+            create and saves a superuser iwth given email and password
+        """
+        user = self.create_user(email, password=password)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(verbose_name='email address' unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    
+    def has_perm(self, perm, obj=None):
+        " Does the user have a specific permission? "
+        #simplest possible answer: yes, always
+        return True
+
+    
+    def has_module_perms(self, app_lebel):
+        "Does the user have permissions to view the app `app_lebel` "
+        #simplest possible answer: yes always
+        return True
